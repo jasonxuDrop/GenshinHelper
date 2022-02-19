08fe74f5201f2d9ef0e3fbbbd65efd67eed9f6a6
Jason Xu <lemonade3532@outlook.com>
2022-02-18 23:15:42 -0800

    First pass on timer edit layout without UI

---
+++ app/src/main/java/com/team11/genshinhelper/ResinTimerEditActivity.kt
@@ -0,0 +1,11 @@
+package com.team11.genshinhelper
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ResinTimerEditActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resin_timer_edit)
+    }
+}
