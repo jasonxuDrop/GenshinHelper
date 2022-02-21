d98304ead2ed8bc30af62a2cd23478e04ac941aa
Jason Xu <lemonade3532@outlook.com>
2022-02-20 22:11:22 -0800

    Added transitions between timer and timer edit activities

--- app/src/main/java/com/team11/genshinhelper/ResinTimerActivity.kt
+++ app/src/main/java/com/team11/genshinhelper/ResinTimerActivity.kt
@@ -1,7 +1,10 @@
 package com.team11.genshinhelper
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 
 class ResinTimerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,5 +12,10 @@ class ResinTimerActivity : AppCompatActivity() {
         setContentView(R.layout.activity_resin_timer)
 
         val door = 3
+
+        findViewById<ImageButton>(R.id.EditButton).setOnClickListener{
+            val intent = Intent(this, ResinTimerEditActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
