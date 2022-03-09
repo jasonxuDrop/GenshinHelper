de0b8becf861bd34beeba5bacc8dfedfc6ed9325
Taha Shaikh <taha_shaikh@sfu.ca>
2022-03-08 19:05:37 -0800

    Added push notification on tap of notification icon as a test

--- app/src/main/java/com/team11/genshinhelper/ResinTimerActivity.kt
+++ app/src/main/java/com/team11/genshinhelper/ResinTimerActivity.kt
@@ -1,12 +1,22 @@
 package com.team11.genshinhelper
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageButton
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 
 class ResinTimerActivity : AppCompatActivity() {
+
+    private val CHANNEL_ID = "channel_id_example_01"
+    private val notificationId = 101
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_resin_timer)
@@ -21,5 +31,39 @@ class ResinTimerActivity : AppCompatActivity() {
             val intent = Intent(this, ListCharacterActivity::class.java)
             startActivity(intent)
         }
+
+        createNotificationChannel()
+
+        val btn_button = findViewById<ImageButton>(R.id.btn_button)
+        btn_button.setOnClickListener{
+            sendNotification()
+        }
     }
+
+    private fun createNotificationChannel(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val name = "Notification Title"
+            val descriptionText = "Notification Description"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID,name,importance).apply {
+            description = descriptionText
+             }
+            val notificationManager: NotificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+    private fun sendNotification(){
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("Resin Timer")
+            .setContentText("You are able to get more Resin!!")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        with(NotificationManagerCompat.from(this)){
+            notify(notificationId, builder.build())
+        }
+    }
+
+
 }
